7b3332e5ab2a7b09dee64b54cf920f3a5ffa62f7
onielrosario <onielrosario@pursuit.org>
2019-01-21 20:19:44 -0500

    connected detail VC outlets

--- WeatherApp/controllers/WeatherDetailController.swift
+++ WeatherApp/controllers/WeatherDetailController.swift
@@ -4,17 +4,34 @@
 //
 //  Created by Oniel Rosario on 1/19/19.
 //  Copyright © 2019 Pursuit. All rights reserved.
-//
+//high, low, sunrise, sunset, windspeed and precipitation
 
 import UIKit
 
 class WeatherDetailController: UIViewController {
+    @IBOutlet weak var backGroundImage: UIImageView!
+    @IBOutlet weak var detailNameandDate: UILabel!
+    @IBOutlet weak var cityImage: UIImageView!
+    @IBOutlet weak var weatherDescription: UILabel!
+    @IBOutlet weak var highF: UILabel!
+    @IBOutlet weak var lowF: UILabel!
+    @IBOutlet weak var sunrise: UILabel!
+    @IBOutlet weak var sunset: UILabel!
+    @IBOutlet weak var windSpeed: UILabel!
+    @IBOutlet weak var precipitation: UILabel!
+    
+    
+    
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
     
+    @IBAction func saveButtonPressed(_ sender: UIBarButtonItem) {
+    }
     
     
 }
